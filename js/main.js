ac7e332a9d04b6ecb794f182d273db4f4a7dc53c
KaoriGBC <kaori.igusa@georgebrown.ca>
2019-04-16 22:50:21 -0400

    edit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-
+// function savePlan(){
+function saveData(){
+//   var myDayPlan = document.getElementById('6:00').value;
+var moodButton = document.getElementById('1').value;
+//   localStorage.setItem(currentDay + '6:00', myDayPlan);
+//
+//   var myDayPlan = document.getElementById('6:30').value;
+//   localStorage.setItem(currentDay + '6:30', myDayPlan);
+// }
+// document.getElementById('savebutton').onclick = function() {
+//   savePlan();
+// }
+}
+//
+// function callPlan(){
+//   var storedPlan = localStorage.getItem(currentDay + '6:00');
+//   document.getElementById('6:00').value = storedPlan;
+//   var storedPlan = localStorage.getItem(currentDay + '6:30');
+//   document.getElementById('6:30').value = storedPlan;
+// }
+//
+// var myTripButton = document.getElementById('datetripb');
+// var myTripDay = document.getElementById('datetrip');
+//
+// function setTrip(){
+//   var myDay = prompt('Please enter your Trip.');
+//   localStorage.setItem('date', myDay);
+//   myTripDay.textContent = myDay
+// }
+//
+// if(!localStorage.getItem('date')) {
+//   setTrip();
+// } else {
+//   var storedDay = localStorage.getItem('date');
+//   myTripDay.textContent = storedDay;
+// }
+//
+// myTripButton.onclick = function() {
+//   setTrip();
+// }
